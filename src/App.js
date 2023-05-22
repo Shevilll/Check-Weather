e298ec615f7ad62a0520fe3a32d6450c7a305a5d
Shevilll <ahmadfaraz00710@gmail.com>
2023-05-22 16:17:37 +0530

    main fetching

--- src/App.js
+++ src/App.js
@@ -1,122 +1,49 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
-let PRODUCTS = [
-    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
-    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
-    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
-];
+const API_KEY = "254b27ccb17d46e5acb95049232205";
+const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=India&aqi=yes`;
 
 export default function App() {
+    return <Getdata URL={URL} />;
+}
+
+function Getdata({ URL }) {
+    const [text, setText] = useState("");
+    const [err, setErr] = useState("");
+    useEffect(() => {
+        fetch(URL)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.error) {
+                    setErr(data.error.message);
+                } else {
+                    setText([
+                        data.location.name,
+                        data.current.condition.text,
+                        data.current.condition.icon,
+                    ]);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [URL]);
+    if (!text) {
+        if (err) {
+            return <h1>{err}</h1>;
+        }
+        return <h1>Searching...</h1>;
+    }
+    return maindata(text);
+}
+function maindata(text) {
     return (
         <>
-            <FilterProducts Items={PRODUCTS} />
+            <h1>{text[0]}</h1>
+            <h1>{text[1]}</h1>
+            <img src={text[2]} />
         </>
     );
-}
-
-function FilterProducts({ Items }) {
-    let [Name, setName] = useState(
-        Items.map((item, index) => (
-            <tr key={index} style={checkStyle(item.stocked)}>
-                <td>{item.name}</td>
-                <td>{item.price}</td>
-                <td>{item.category}</td>
-            </tr>
-        ))
-    );
-    return (
-        <div>
-            <div>
-                <form>
-                    <input
-                        type="search"
-                        placeholder="Search"
-                        onChange={(e) =>
-                            searching(e.target.value, setName, Items)
-                        }
-                    />
-                    <br />
-                    <label>
-                        <input
-                            type="checkbox"
-                            onChange={(e) =>
-                                checking(e.target.checked, setName, Items)
-                            }
-                        />{" "}
-                        Only show products in stock.
-                    </label>
-                </form>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Category</th>
-                    </tr>
-                </thead>
-                <tbody>{Name}</tbody>
-            </table>
-        </div>
-    );
-}
-
-function checkStyle(check) {
-    if (!check) {
-        return { color: "red" };
-    } else {
-        return;
-    }
-}
-
-function checking(e, setName, Items) {
-    if (e === true) {
-        setName(
-            Items.filter((item) => item.stocked).map((item, index) => (
-                <tr key={index} style={checkStyle(item.stocked)}>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.category}</td>
-                </tr>
-            ))
-        );
-    } else {
-        setName(
-            Items.map((item, index) => (
-                <tr key={index} style={checkStyle(item.stocked)}>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.category}</td>
-                </tr>
-            ))
-        );
-    }
-}
-function searching(e, setName, Items) {
-    if (e) {
-        setName(
-            Items.filter((item) =>
-                item.name.toLowerCase().includes(e.toLowerCase())
-            ).map((item, index) => (
-                <tr key={index} style={checkStyle(item.stocked)}>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.category}</td>
-                </tr>
-            ))
-        );
-    } else {
-        setName(
-            Items.map((item, index) => (
-                <tr key={index} style={checkStyle(item.stocked)}>
-                    <td>{item.name}</td>
-                    <td>{item.price}</td>
-                    <td>{item.category}</td>
-                </tr>
-            ))
-        );
-    }
 }
