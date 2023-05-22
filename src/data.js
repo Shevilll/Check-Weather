23a77e0b0ee5c9122016c8bd5814cce5f8d7080f
Shevilll <ahmadfaraz00710@gmail.com>
2023-05-22 21:33:00 +0530

    all done

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { maindata } from "./main";
+import { handleError } from "./main";
 
 export function Getdata({ URL }) {
     const [text, setText] = useState("");
@@ -14,23 +15,17 @@ export function Getdata({ URL }) {
                     setErr(data.error.message);
                 } else {
                     setErr("");
-                    setText([
-                        data.location.name,
-                        data.current.condition.text,
-                        data.current.condition.icon,
-                    ]);
+                    setText({
+                        data,
+                    });
                 }
             })
             .catch((error) => {
                 console.log(error);
             });
     }, [URL]);
-    if (err === "Parameter q is missing.") {
-        return <h1>Enter Location.</h1>;
-    } else if (err === "No matching location found.") {
-        return <h1>{err}</h1>;
-    } else if (!text) {
-        return <h1>Searching...</h1>;
+    if (err) {
+        return handleError(err);
     }
     return maindata(text);
 }
